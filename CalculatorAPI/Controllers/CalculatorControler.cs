d261ba4c3a1071baf3cb218edee7074d0dd894f6
Emanuel Maluleke <emanuelm@bbd.co.za>
2022-06-07 17:41:04 +0200

    Create Calculator API project

---
+++ CalculatorAPI/Controllers/CalculatorControler.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorControler : ControllerBase
+    {
+        private static readonly string[] BODMAS = new[]
+        {
+            "Brackets", "Order", "Division", "Multiplication", "Addition", "Subtraction"
+        };
+
+        private readonly ILogger<CalculatorControler> _logger;
+
+        public CalculatorControler(ILogger<CalculatorControler> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<String> Get()
+        {
+
+            return BODMAS.ToArray();
+        }
+    }
+}
