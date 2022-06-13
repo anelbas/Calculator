2c07edea98c1fb1f306161354b8f3f9fe05e20e1
Emanuel Maluleke <emanuelm@bbd.co.za>
2022-06-13 16:43:33 +0200

    #2 Add Authorization on the swagger

--- CalculatorAPI/Program.cs
+++ CalculatorAPI/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using Microsoft.OpenApi.Models;
 using CalculatorAPI;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,7 +11,30 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(
+    c =>{
+       
+        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme{
+            In = ParameterLocation.Header,
+            Description = "Please insert Token",
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            BearerFormat = "JWT",
+            Scheme = "bearer"
+        });
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement{
+            {
+                new OpenApiSecurityScheme{
+                    Reference = new OpenApiReference{
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                new string[]{}
+                }
+         });
+    }
+);
 /// <summary>
 var key = "CalculatorAPIAuth12345%%%%%$$$";
 
