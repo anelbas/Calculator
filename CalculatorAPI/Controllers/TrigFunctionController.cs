2fa773ace9aea9e850c022866e5ff6d64d196bfe
BawinileM <bawinile.mqwebedu@gmail..com>
2022-06-14 15:05:55 +0200

    #3 error handling

--- CalculatorAPI/Controllers/TrigFunctionController.cs
+++ CalculatorAPI/Controllers/TrigFunctionController.cs
@@ -7,22 +7,29 @@ using CalculatorAPI.Repository;
 using Microsoft.AspNetCore.Authorization;
 
 
+
 namespace CalculatorAPI.Controllers
 {   
     public class TrigFunctionController
     {     [Authorize]
           [HttpPost("TrigFunctions")]
-        public async Task<IActionResult> getInputs([FromBody] TrigPostRequest trig)
+        public Task<IActionResult> getInputs([FromBody] TrigPostRequest trig)
         {
         
-            
-            TrigPostRequest d = new  TrigPostRequest();
-            TrigFunctionRepository trigfun = new TrigFunctionRepository();
-            List<Trig> ls= trig.Tags;
-            return new ObjectResult(trigfun.TrigImplement(ls));
-       
-        }
+            try{
+                TrigPostRequest d = new  TrigPostRequest();
+                TrigFunctionRepository trigfun = new TrigFunctionRepository();
+                List<Trig> ls= trig.Tags;
+                return new ObjectResult(trigfun.TrigImplement(ls));
+            }
+            catch(ErrorException ex){
+                Response.StatusCode = ex.StatusCode;
+                return ex.ToString();
+
+            }
+    
 
           
     }
+}
 }
